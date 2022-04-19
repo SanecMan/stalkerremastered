d3e89f2414e8a87d1850dead484e10578a50db85
Sanec <xxxxxxxxxxxxxxxxxxxx.2002@gmail.com>
2022-04-19 19:11:43 +0700

    Update STALKER_START.bash

--- STALKER_START.bash
+++ STALKER_START.bash
@@ -6,6 +6,5 @@ d=$(dirname $0)
 bash ${d}/STALKER_UPDATE.bash;
 echo "Компилим";
 sudo DreamMaker stalker.dme;
-echo "Поднимаем";
-sudo DreamDaemon stalker.dmb 2013 -trusted -logself &;
-echo "Сталкер поднят на 2013 порту, энжой.";
+echo "Поднимаемна 2013 порту";
+sudo DreamDaemon stalker.dmb 2013 -trusted -logself &
